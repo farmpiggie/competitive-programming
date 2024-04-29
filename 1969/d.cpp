5966d774281e38e2fc3b6eab1e2e7618d4564673
Wesley Hung <whung@ucsb.edu>
2024-04-29 17:12:11 +0000

    rip

---
+++ 1969/d.cpp
@@ -0,0 +1,15 @@
+// bob always chooses max(0, (subset) - k) cheapest items to buy (k[i])
+// if alice chooses <= k items, her profit is negative
+// she wants to choose items that are expensive in a[j] so that bob needs to buy them?
+// (subset - k) cheapest items in a[j] should try to offset prices of all other items
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	int t; cin >> t;
+	while (t--) {
+
+	}
+	return 0;
+}
